3a74f704fcdd214b56562d9b83a1d7215b307d75
Sebastian Alberternst <sebastian.alberternst@dfki.de>
2023-09-13 16:08:11 +0000

    Another fix for the thingsboard url

--- frontend/src/pages/thingsboard.js
+++ frontend/src/pages/thingsboard.js
@@ -1,16 +1,20 @@
 import React from "react";
 import Layout from "@theme/Layout";
-import BrowserOnly from '@docusaurus/BrowserOnly';
 
 export default function Thingsboard() {
+    const [url, setUrl] = React.useState()
+
+    React.useEffect(() => {
+        setUrl(new URL(window.location.href))
+    }, [setUrl]);
+
+    if(url === undefined) {
+        return null
+    }
+    
     return (
         <Layout title="Thingsboard" description="Landing Page">
-            <BrowserOnly>
-                {() => {
-                    const url = new URL(window.location.href)
-                    return <iframe src={`${url.protocol}://thingsboard.${url.host}/`} style={{ position: "absolute", width: "100%", height: "100%" }} />
-                }}
-            </BrowserOnly>
+            <iframe src={`${url.protocol}//thingsboard.${url.host}/`} style={{ position: "absolute", width: "100%", height: "100%" }} />
         </Layout>
     );
 }
