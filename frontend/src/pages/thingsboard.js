96eab4e97ce5a4e205ce5dbc375d1179fb938fe7
Sebastian Alberternst <sebastian.alberternst@dfki.de>
2023-09-13 15:48:47 +0000

    Render link to thingsboard on client

--- frontend/src/pages/thingsboard.js
+++ frontend/src/pages/thingsboard.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Layout from "@theme/Layout";
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 export default function Thingsboard() {
-    const url = new URL(window.location.href)
     return (
         <Layout title="Thingsboard" description="Landing Page">
-            <iframe src={`${url.protocol}://thingsboard.${url.host}/`} style={{ position: "absolute", width: "100%", height: "100%" }} />
+            <BrowserOnly>
+                {() => {
+                    const url = new URL(window.location.href)
+                    return <iframe src={`${url.protocol}://thingsboard.${url.host}/`} style={{ position: "absolute", width: "100%", height: "100%" }} />
+                }}
+            </BrowserOnly>
         </Layout>
     );
 }
