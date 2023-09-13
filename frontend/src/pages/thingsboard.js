c77eceaafb4fdc6ebc871b575cf2e8bb138b117a
Sebastian Alberternst <sebastian.alberternst@dfki.de>
2023-09-13 15:33:13 +0000

    Fix link to thingsboard

--- frontend/src/pages/thingsboard.js
+++ frontend/src/pages/thingsboard.js
@@ -2,9 +2,10 @@ import React from "react";
 import Layout from "@theme/Layout";
 
 export default function Thingsboard() {
+    const url = new URL(window.location.href)
     return (
         <Layout title="Thingsboard" description="Landing Page">
-            <iframe src="http://thingsboard.example-tenant.192-168-178-60.nip.io/" style={{ position: "absolute", width: "100%", height: "100%" }} />
+            <iframe src={`${url.protocol}://thingsboard.${url.host}/`} style={{ position: "absolute", width: "100%", height: "100%" }} />
         </Layout>
     );
 }
