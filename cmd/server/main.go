480e91dea0534a3a645c33ae75674ed993fe6e05
Sebastian Alberternst <sebastian.alberternst@dfki.de>
2024-07-07 08:30:39 +0000

    Serve config.js

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -11,6 +11,7 @@ func main() {
 	r.StaticFile("/", "./public/index.html")
 	r.StaticFile("/manifest.json", "./public/manifest.json")
 	r.StaticFile("/cropped-android-chrome-512x512-1-1-192x192.png", "./public/cropped-android-chrome-512x512-1-1-192x192.png")
+	r.StaticFile("/config.js", "./public/config.js")
 	r.Static("/assets", "./public/assets")
 
 	routes.AddRoutes(r)
